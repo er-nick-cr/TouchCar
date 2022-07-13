4391596f900ddbf120104dc9eacb1800ba7da8a2
er-nick-cr <nikiteremeev2010@gmail.com>
2022-07-13 19:38:47 +0700

    feature/add detailed part bottom sheet

--- feature-parts/src/main/java/com/example/feature_parts/component/ComponentFragment.kt
+++ feature-parts/src/main/java/com/example/feature_parts/component/ComponentFragment.kt
@@ -20,6 +20,9 @@ import com.example.feature_parts.R
 import com.example.feature_parts.component.items_recycler.ComponentItemsAdapter
 import com.example.feature_parts.component.selector_recycler.SelectorAdapter
 import com.example.feature_parts.databinding.ComponentFragmentBinding
+import com.example.feature_parts.detailed_part.DetailedPartFragment
+import com.example.feature_parts.utils.BottomSheetCallback
+import com.example.feature_parts.utils.includes
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -115,16 +118,20 @@ class ComponentFragment : Fragment() {
         recyclerView.addItemDecoration(dividerItemDecoration)
     }
 
-    fun onClickImage(selectedCoordinates: SelectedCoordinates) {
-        viewModel.itemsLiveData.observe(this) { items ->
-            val index = items.indexOfFirst {item ->  selectedCoordinates.x in item.coordinates.x1..item.coordinates.x2 &&
-                        selectedCoordinates.y in item.coordinates.y1..item.coordinates.y2 }
-            if (index != -1) {
-                componentItemAdapter.onItemSelectedByCoordinates(index)
-                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-                binding.componentRecycler.scrollToPosition(index)
+    private fun onClickImage(selectedCoordinates: SelectedCoordinates) {
+        val invertedSelectedCoordinates = SelectedCoordinates(
+            x = selectedCoordinates.x,
+            y = selectedCoordinates.y
+        )
+        viewModel.currentItemsLiveData.value
+            ?.indexOfFirst { item -> item.coordinates.includes(invertedSelectedCoordinates) }
+            ?.let { index ->
+                if (index != -1) {
+                    componentItemAdapter.onItemSelectedByCoordinates(invertedSelectedCoordinates)
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                    binding.componentRecycler.scrollToPosition(index)
+                }
             }
-        }
     }
 
     private fun onSelectorItemClick(index: Int) {
